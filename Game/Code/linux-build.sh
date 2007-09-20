7b59fef5287d0b48948f28baa6faa5e35bc27b65
jay binks <jaybinks@users.sourceforge.net>
2007-09-20 06:38:52 +0000

    adding my helper script :) sorry bout the absolute paths.. ( will fix it up.. promise :P )
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@410 b956fd51-792f-4845-bead-9b4dfca2ff2c

---
+++ Game/Code/linux-build.sh
@@ -0,0 +1,4 @@
+clear
+fpc  -S2cgi -OG1 -gl -vewnhi -l -Filib/JEDI-SDLv1.0/SDL/Pas/ -Fu/usr/bin/lazarus/components/images/lib/i386-linux/ -Fu/usr/bin/lazarus/lcl/units/i386-linux/ -Fu/usr/bin/lazarus/lcl/units/i386-linux/gtk2/ -Fu/usr/bin/lazarus/packager/units/i386-linux/ -Fu. -oUltraStar -dLCL -dLCLgtk2 UltraStar.lpr
+
+mv ./UltraStar /home/jay/src/ultrastardx/output/
