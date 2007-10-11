470ce7727fa8e94f06470bc8c6ea3d2677f0ca29
jay binks <jaybinks@users.sourceforge.net>
2007-10-11 13:41:38 +0000

    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@507 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- Game/Code/bamboo-build-win-delphi.bat
+++ Game/Code/bamboo-build-win-delphi.bat
@@ -1,1 +1,7 @@
 "C:\Program Files\Borland\BDS\4.0\Bin\dcc32.exe" -U"lib\JEDI-SDLv1.0\SDL\Pas" -O"lib\JEDI-SDLv1.0\SDL\Pas" -I"lib\JEDI-SDLv1.0\SDL\Pas" -R"lib\JEDI-SDLv1.0\SDL\Pas" UltraStar.dpr
+cp UltraStar.exe ..\..\
+
+cd ..\..\Installer
+"C:\Program Files\NSIS\makeNSIS.exe" UltraStarDeluxe.nsi
+
+cd ..\Game\Code
