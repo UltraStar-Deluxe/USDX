5318f2a8ebdc199f3f2b27b76ea04ac3a3f050c2
Eddie <eddie-0815@users.sourceforge.net>
2007-11-08 21:09:23 +0000

    Added missing functions. I Hope this fixes the build on Windows.
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@596 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- Game/Code/Classes/UPlatformWindows.pas
+++ Game/Code/Classes/UPlatformWindows.pas
@@ -11,6 +11,16 @@ interface
 uses Classes, UPlatform;
 
 type
+  
+  TSearchRecW = record
+    Time: Integer;
+    Size: Integer;
+    Attr: Integer;
+    Name: WideString;
+    ExcludeAttr: Integer;
+    FindHandle: THandle;
+    FindData: TWin32FindDataW;
+  end;
 
   TPlatform = class(TInterfacedObject, IPlatform)
   public
@@ -20,6 +30,66 @@ type
 implementation
 
 uses SysUtils, Windows;
+
+function FindFirstW(const Path: widestring; Attr: Integer; var  F: TSearchRecW): Integer;
+const
+  faSpecial = faHidden or faSysFile or faVolumeID or faDirectory;
+begin
+  F.ExcludeAttr := not Attr and faSpecial;
+  F.FindHandle  := FindFirstFileW(PWideChar(Path), F.FindData);
+  if F.FindHandle <> INVALID_HANDLE_VALUE then
+  begin
+    Result := FindMatchingFileW(F);
+    if Result <> 0 then FindCloseW(F);
+  end else
+    Result := GetLastError;
+end;
+
+function FindNextW(var F: TSearchRecW): Integer;
+begin
+  if FindNextFileW(F.FindHandle, F.FindData) then
+    Result := FindMatchingFileW(F)
+  else
+    Result := GetLastError;
+end;
+
+procedure FindCloseW(var F: TSearchRecW);
+begin
+  if F.FindHandle <> INVALID_HANDLE_VALUE then
+  begin
+    Windows.FindClose(F.FindHandle);
+    F.FindHandle := INVALID_HANDLE_VALUE;
+  end;
+end;
+
+function FindMatchingFileW(var F: TSearchRecW): Integer;
+var
+  LocalFileTime: TFileTime;
+begin
+  with F do
+  begin
+    while FindData.dwFileAttributes and ExcludeAttr <> 0 do
+      if not FindNextFileW(FindHandle, FindData) then
+      begin
+        Result := GetLastError;
+        Exit;
+      end;
+    FileTimeToLocalFileTime(FindData.ftLastWriteTime, LocalFileTime);
+    FileTimeToDosDateTime(LocalFileTime, LongRec(Time).Hi, LongRec(Time).Lo);
+    Size := FindData.nFileSizeLow;
+    Attr := FindData.dwFileAttributes;
+    Name := FindData.cFileName;
+  end;
+  Result := 0;
+end;
+
+function DirectoryExistsW(const Directory: widestring): Boolean;
+var
+  Code: Integer;
+begin
+  Code := GetFileAttributesW(PWideChar(Directory));
+  Result := (Code <> -1) and (FILE_ATTRIBUTE_DIRECTORY and Code <> 0);
+end;
 
 Function TPlatform.DirectoryFindFiles(Dir, Filter : WideString; ReturnAllSubDirs : Boolean) : TDirectoryEntryArray; 
 var
