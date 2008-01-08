bb4df27d207ffb6382e332843672c6c75bab0d9b
jay binks <jaybinks@users.sourceforge.net>
2008-01-08 12:22:50 +0000

    this script seems to build a deb now... wonder if its correct :)
    or if it even works !! 
    haha
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@759 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- Game/Code/package_debian.sh
+++ Game/Code/package_debian.sh
@@ -2,9 +2,13 @@
 # and i should move files to the correct location for packaging ... ( for DEB package )
 
 rm -fr ../../../deb-package
+rm -fr ../../../packages
 clear
 
+mkdir ../../../packages
+
 mkdir ../../../deb-package
+mkdir ../../../deb-package/DEBIAN
 mkdir ../../../deb-package/usr
 mkdir ../../../deb-package/usr/share
 mkdir ../../../deb-package/usr/share/UltraStarDeluxe
@@ -16,3 +20,12 @@ cp -a ../../Themes/ ../../../deb-package/usr/share/UltraStarDeluxe/
 cp -a ../../Sounds/ ../../../deb-package/usr/share/UltraStarDeluxe/
 cp -a ../../Skins/ ../../../deb-package/usr/share/UltraStarDeluxe/
 cp -a ../../Languages/ ../../../deb-package/usr/share/UltraStarDeluxe/
+
+cp UltraStarDeluxe.control ../../../deb-package/DEBIAN/control
+
+cd ../../../
+
+dpkg-deb --build ./deb-package
+mv deb-package.deb ./packages/UltraStarDeluxe_1.1_i386.deb
+
+rm -fr ../../../deb-package
