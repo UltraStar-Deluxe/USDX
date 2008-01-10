7bdab886d2b0483acc5904c9cfa8cc5ba3ae99ef
Tobias Gunkel <tobigun@users.sourceforge.net>
2008-01-10 23:26:03 +0000

    "MATHEMATICS.pas" is now "mathematics.pas"
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@777 b956fd51-792f-4845-bead-9b4dfca2ff2c

---
+++ Game/Code/lib/ffmpeg/mathematics.pas
@@ -0,0 +1,79 @@
+unit mathematics;
+
+interface
+
+(*
+ * copyright (c) 2005 Michael Niedermayer <michaelni@gmx.at>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+(* This is a part of Pascal porting of ffmpeg.  Originally by Victor Zinetz for Delphi and Free Pascal on Windows.
+For Mac OS X, some modifications were made by The Creative CAT, denoted as CAT
+in the source codes *)
+
+{$IFDEF FPC}
+  {$IFDEF LINUX}
+  {$LINKLIB libavutil}
+  {$ENDIF}
+  {$MODE DELPHI } (* CAT *)
+  {$PACKENUM 4}    (* every enum type variables uses 4 bytes, CAT *)
+  {$PACKRECORDS C}    (* GCC compatible, Record Packing, CAT *)
+{$ENDIF}
+
+uses
+  rational; (* CAT *)
+
+const
+{$IFDEF MSWINDOWS}
+  av__util = 'avutil-49.dll';
+{$ENDIF}
+{$IFDEF LINUX}
+  av__util = 'libavutil.so';   // .0d
+{$ENDIF}
+{$IFDEF DARWIN}
+  av__util = 'libavutil.dylib'; 
+{$ENDIF}
+
+type
+  TAVRounding = (
+    AV_ROUND_ZERO     = 0, ///< round toward zero
+    AV_ROUND_INF      = 1, ///< round away from zero
+    AV_ROUND_DOWN     = 2, ///< round toward -infinity
+    AV_ROUND_UP       = 3, ///< round toward +infinity
+    AV_ROUND_NEAR_INF = 5, ///< round to nearest and halfway cases away from zero
+    AV_ROUND_FUCKING = $FFFFFF
+  );
+
+(** * rescale a 64bit integer with rounding to nearest.
+ * a simple a*b/c isn't possible as it can overflow *)
+function av_rescale (a, b, c: int64): int64;
+  cdecl; external av__util;
+
+(**
+ * rescale a 64bit integer with specified rounding.
+ * a simple a*b/c isn't possible as it can overflow *)
+function av_rescale_rnd (a, b, c: int64; enum: TAVRounding): int64;
+  cdecl; external av__util;
+
+(**
+ * rescale a 64bit integer by 2 rational numbers. *)
+function av_rescale_q (a: int64; bq, cq: TAVRational): int64;
+  cdecl; external av__util;
+
+implementation
+
+end.
+ 
