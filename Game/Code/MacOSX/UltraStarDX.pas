fd41fd87b334ee247d585477cde28f43f54a9384
Eddie <eddie-0815@users.sourceforge.net>
2008-01-11 20:56:16 +0000

    Fixed compiling and typos on OS X.
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@788 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- Game/Code/MacOSX/UltraStarDX.pas
+++ Game/Code/MacOSX/UltraStarDX.pas
@@ -1,6 +1,6 @@
 program UltraStarDX;
 
-uses cthreads, UMain, UMedia_dummy, UAudio_bass, USingNotes, UTextClasses, UVideo;
+uses cthreads, UMain, UMedia_dummy, UAudioPlayback_Bass, UAudioInput_Bass, USingNotes, UTextClasses, UVideo;
 
 begin
   Main;
