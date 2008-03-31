8358236dfc75ed0bc9350f6a002dfabb6f1f470e
Tobias Gunkel <tobigun@users.sourceforge.net>
2008-03-31 11:59:15 +0000

    Support for Unicode-strings and chars (etc. WideUpperCase) in MacOSX.
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@986 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- Game/Code/MacOSX/UltraStarDX.pas
+++ Game/Code/MacOSX/UltraStarDX.pas
@@ -1,6 +1,6 @@
 program UltraStarDX;
 
-uses cthreads, UMain, UMedia_dummy, UAudioPlayback_Bass, UAudioInput_Bass, USingNotes, UTextClasses, UVideo;
+uses cthreads, cwstring, UMain, UMedia_dummy, UAudioPlayback_Bass, UAudioInput_Bass, USingNotes, UTextClasses, UVideo;
 
 begin
   Main;
