9dd5813e9fef19a3a0ee09a4331f297d1dd4a072
mischi <karl-michael.schindler@web.de>
2008-05-24 08:37:23 +0000

    finally removed special version of the function PosEx for darwin. Not needed
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@1131 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- Game/Code/Menu/UMenuText.pas
+++ Game/Code/Menu/UMenuText.pas
@@ -64,41 +64,6 @@ implementation
 uses UGraphic,
      StrUtils;
 
-{$IFDEF DARWIN}	 
-{ Why is this needed? Is PosEx from StrUtils different?
-function PosEx(const SubStr, S: string; Offset: Cardinal = 1): integer;
-var
-  I, X: integer;
-  Len, LenSubStr: integer;
-begin
-  if Offset = 1 then
-    Result := Pos(SubStr, S)
-  else
-  begin
-    I := Offset;
-    LenSubStr := Length(SubStr);
-    Len := Length(S) - LenSubStr + 1;
-    while I <= Len do
-    begin
-      if S[I] = SubStr[1] then
-      begin
-        X := 1;
-        while (X < LenSubStr) and (S[I + X] = SubStr[X + 1]) do
-          Inc(X);
-        if (X = LenSubStr) then
-        begin
-          Result := I;
-          exit;
-        end;
-      end;
-      Inc(I);
-    end;
-    Result := 0;
-  end;
-end;
-}
-{$ENDIF}	 
-
 procedure TText.SetSelect(Value: boolean);
 begin
   SelectBool := Value;
