f4a47d46dce60bf1a042230140bd200ed02429cc
Tobias Gunkel <tobigun@users.sourceforge.net>
2008-08-27 11:35:30 +0000

    USDXResCompiler was replaced by ResourceExtractor
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@1300 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- Tools/USDXResCompiler/linux-build.sh
+++
@@ -1,3 +0,0 @@
-clear
-fpc  -S2cgi -OG1 -gl -vewnhi -l -Filib/JEDI-SDLv1.0/SDL/Pas/ -Fu/usr/lib/lazarus/components/images/lib/i386-linux/ -Fu/usr/lib/lazarus/lcl/units/i386-linux/ -Fu/usr/lib/lazarus/lcl/units/i386-linux/gtk2/ -Fu/usr/lib/lazarus/packager/units/i386-linux/ -Fu. -dLCL -dLCLgtk2 USDXResCompiler.lpr
-
