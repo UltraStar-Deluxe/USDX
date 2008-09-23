0c971bce0b3d6130e76b52d2617a09f170352737
Tobias Gunkel <tobigun@users.sourceforge.net>
2008-09-23 21:17:50 +0000

    added file headers
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@1404 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- src/media/UVideo.pas
+++ src/media/UVideo.pas
@@ -1,16 +1,36 @@
-{##############################################################################
- #                    FFmpeg support for UltraStar deluxe                     #
- #                                                                            #
- #    Created by b1indy                                                       #
- #    based on 'An ffmpeg and SDL Tutorial' (http://www.dranger.com/ffmpeg/)  #
- #    with modifications by Jay Binks <jaybinks@gmail.com>                    #
- #                                                                            #
- # http://www.mail-archive.com/fpc-pascal@lists.freepascal.org/msg09949.html  #
- # http://www.nabble.com/file/p11795857/mpegpas01.zip                         #
- #                                                                            #
- ##############################################################################}
-
+{* UltraStar Deluxe - Karaoke Game
+ *
+ * UltraStar Deluxe is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING. If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * $URL$
+ * $Id$
+ *}
+ 
 unit UVideo;
+
+{*
+ * based on 'An ffmpeg and SDL Tutorial' (http://www.dranger.com/ffmpeg/)
+ *
+ * http://www.mail-archive.com/fpc-pascal@lists.freepascal.org/msg09949.html
+ * http://www.nabble.com/file/p11795857/mpegpas01.zip
+ *}
 
 // uncomment if you want to see the debug stuff
 {.$define DebugDisplay}
