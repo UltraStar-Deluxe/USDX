fab36d86b55b9f76bfb3ff932e2b2b9886f1e02f
Whiteshark <whiteshark0@users.sourceforge.net>
2009-02-28 22:24:59 +0000

    some cosmetic changes
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@1612 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- src/base/UMusic.pas
+++ src/base/UMusic.pas
@@ -39,10 +39,14 @@ uses
   Classes;
 
 type
-  TNoteType = (ntFreestyle = 0, ntNormal = 1, ntGolden = 2);
+  TNoteType = (ntFreestyle, ntNormal, ntGolden);
 
 const
-  //Score Factor
+  // ScoreFactor defines how a notehit of a specified notetype is
+  // measured in comparison to the other types
+  // 0 means this notetype is not rated at all
+  // 2 means a hit of this notetype will be rated w/ twice as much
+  // points as a hit of a notetype w/ ScoreFactor 1
   ScoreFactor:         array[TNoteType] of integer = (0, 1, 2);
 
 type
