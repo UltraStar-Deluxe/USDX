ad6c78951dda85abfd0a8c0f542e7ecf7c861041
mischi <karl-michael.schindler@web.de>
2010-05-03 21:58:30 +0000

    ESC saves again. revert 1183.
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@2338 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- src/screens/UScreenOptionsGraphics.pas
+++ src/screens/UScreenOptionsGraphics.pas
@@ -76,8 +76,7 @@ begin
       SDLK_ESCAPE,
       SDLK_BACKSPACE :
         begin
-          // Escape -> save nothing - just leave this screen
-          
+          Ini.Save;
           AudioPlayback.PlaySound(SoundLib.Back);
           FadeTo(@ScreenOptions);
         end;
