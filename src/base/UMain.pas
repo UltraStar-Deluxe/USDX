3ed5b47f80321fc50c5c8dcbf002bd2496879fe6
Whiteshark <whiteshark0@users.sourceforge.net>
2010-06-10 20:12:08 +0000

    fix compiling after r2478
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@2480 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- src/base/UMain.pas
+++ src/base/UMain.pas
@@ -358,7 +358,7 @@ var
 begin
   SDL_EnableKeyRepeat(125, 125);
 
-  Continue := true;
+  Done := true;
 
   CountSkipTime();  // JB - for some reason this seems to be needed when we use the SDL Timer functions.
   while not Done do
