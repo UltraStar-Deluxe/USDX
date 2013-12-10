321f4ff972a6b76cdcb426cd24148b49ecef4607
mischi <karl-michael.schindler@web.de>
2013-12-10 22:47:26 +0000

    Create ffmpeg-2.1 from ffmpeg-2.0. Plain copy, no adjustment
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@3024 b956fd51-792f-4845-bead-9b4dfca2ff2c

---
+++ src/lib/ffmpeg-2.1/libavutil/log.pas
@@ -0,0 +1,244 @@
+(*
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * This is a part of the Pascal port of ffmpeg.
+ * - Changes and updates by the UltraStar Deluxe Team
+ *
+ * Conversion of libavutil/log.h
+ * avutil version 52.38.100
+ *
+ *)
+
+(**
+ * @file
+ * log
+ *)
+
+type
+  PAVClassCategory = ^TAVClassCategory;
+  TAVClassCategory = (
+    AV_CLASS_CATEGORY_NA = 0,
+    AV_CLASS_CATEGORY_INPUT,
+    AV_CLASS_CATEGORY_OUTPUT,
+    AV_CLASS_CATEGORY_MUXER,
+    AV_CLASS_CATEGORY_DEMUXER,
+    AV_CLASS_CATEGORY_ENCODER,
+    AV_CLASS_CATEGORY_DECODER,
+    AV_CLASS_CATEGORY_FILTER,
+    AV_CLASS_CATEGORY_BITSTREAM_FILTER,
+    AV_CLASS_CATEGORY_SWSCALER,
+    AV_CLASS_CATEGORY_SWRESAMPLER,
+    AV_CLASS_CATEGORY_NB ///< not part of ABI/API
+  );
+
+// struct AVOptionRanges;
+
+(**
+ * Describe the class of an AVClass context structure. That is an
+ * arbitrary struct of which the first field is a pointer to an
+ * AVClass struct (e.g. AVCodecContext, AVFormatContext etc.).
+ *)
+  PAVClass = ^TAVClass;
+  TAVClass = record
+    (**
+     * The name of the class; usually it is the same name as the
+     * context structure type to which the AVClass is associated.
+     *)
+    class_name: PAnsiChar;
+
+    (**
+     * A pointer to a function which returns the name of a context
+     * instance ctx associated with the class.
+     *)
+    item_name: function(ctx: pointer): PAnsiChar; cdecl;
+
+    (**
+     * a pointer to the first option specified in the class if any or NULL
+     *
+     * @see av_set_default_options()
+     *)
+    option: PAVOption;
+
+    (**
+     * LIBAVUTIL_VERSION with which this structure was created.
+     * This is used to allow fields to be added without requiring major
+     * version bumps everywhere.
+     *)
+    version: cint;
+
+    (**
+     * Offset in the structure where log_level_offset is stored.
+     * 0 means there is no such variable
+     *)
+    log_level_offset_offset: cint;
+
+    (**
+     * Offset in the structure where a pointer to the parent context for
+     * logging is stored. For example a decoder could pass its AVCodecContext
+     * to eval as such a parent context, which an av_log() implementation
+     * could then leverage to display the parent context.
+     * The offset can be NULL.
+     *)
+    parent_log_context_offset: cint;
+    
+    (**
+     * Return next AVOptions-enabled child or NULL
+     *)
+    child_next: function (obj: pointer; prev: pointer): pointer; cdecl;
+
+    (**
+     * Return an AVClass corresponding to the next potential
+     * AVOptions-enabled child.
+     *
+     * The difference between child_next and this is that
+     * child_next iterates over _already existing_ objects, while
+     * child_class_next iterates over _all possible_ children.
+     *)
+    child_class_next: function (prev: {const} PAVClass): {const} PAVClass; cdecl;
+
+    (**
+     * Category used for visualization (like color)
+     * This is only set if the category is equal for all objects using this class.
+     * available since version (51 << 16 | 56 << 8 | 100)
+     *)
+    category: TAVClassCategory;
+
+    (**
+     * Callback to return the category.
+     * available since version (51 << 16 | 59 << 8 | 100)
+     *)
+    get_category: function (ctx: pointer): PAVClassCategory; cdecl;
+
+    (**
+     * Callback to return the supported/allowed ranges.
+     * available since version (52.12)
+     *)
+    query_ranges: function (P: PPAVOptionRanges; obj: pointer; key: {const} PAnsiChar; flags: cint): cint; cdecl;
+end;
+
+const
+  AV_LOG_QUIET   = -8;
+
+(**
+ * Something went really wrong and we will crash now.
+ *)
+  AV_LOG_PANIC   =  0;
+
+(**
+ * Something went wrong and recovery is not possible.
+ * For example, no header was found for a format which depends
+ * on headers or an illegal combination of parameters is used.
+ *)
+  AV_LOG_FATAL   =  8;
+
+(**
+ * Something went wrong and cannot losslessly be recovered.
+ * However, not all future data is affected.
+ *)
+  AV_LOG_ERROR   = 16;
+
+(**
+ * Something somehow does not look correct. This may or may not
+ * lead to problems. An example would be the use of '-vstrict -2'.
+ *)
+  AV_LOG_WARNING = 24;
+
+  AV_LOG_INFO    = 32;
+  AV_LOG_VERBOSE = 40;
+
+(**
+ * Stuff which is only useful for libav* developers.
+ *)
+  AV_LOG_DEBUG   = 48;
+
+  AV_LOG_MAX_OFFSET = (AV_LOG_DEBUG - AV_LOG_QUIET);
+
+(**
+ * Send the specified message to the log if the level is less than or equal
+ * to the current av_log_level. By default, all logging messages are sent to
+ * stderr. This behavior can be altered by setting a different av_vlog callback
+ * function.
+ *
+ * @param avcl A pointer to an arbitrary struct of which the first field is a
+ * pointer to an AVClass struct.
+ * @param level The importance level of the message, lower values signifying
+ * higher importance.
+ * @param fmt The format string (printf-compatible) that specifies how
+ * subsequent arguments are converted to output.
+ * @see av_vlog
+ *)
+
+{** to be translated if needed
+void av_log(void *avcl, int level, const char *fmt, ...) av_printf_format(3, 4);
+**}
+
+type
+  va_list = pointer;
+
+procedure av_vlog(avcl: pointer; level: cint; fmt: {const} PAnsiChar; dummy: va_list);
+  cdecl; external av__util;
+function av_log_get_level(): cint;
+  cdecl; external av__util;
+procedure av_log_set_level(level: cint);
+  cdecl; external av__util;
+
+{** to be translated if needed
+void av_log_set_callback(void (*)(void*, int, const char*, va_list));
+void av_log_default_callback(void* ptr, int level, const char* fmt, va_list vl);
+**}
+
+function av_default_item_name (ctx: pointer): PAnsiChar;
+  cdecl; external av__util;
+function av_default_get_category(ptr: pointer): TAVClassCategory;
+  cdecl; external av__util;
+
+(**
+ * Format a line of log the same way as the default callback.
+ * @param line          buffer to receive the formated line
+ * @param line_size     size of the buffer
+ * @param print_prefix  used to store whether the prefix must be printed;
+ *                      must point to a persistent integer initially set to 1
+ *)
+procedure av_log_format_line(ptr: pointer; level: cint; fmt: {const} PAnsiChar; vl: va_list;
+                        line: PAnsiChar; line_size: cint; print_prefix: Pcint);
+  cdecl; external av__util;
+
+(**
+ * av_dlog macros
+ * Useful to print debug messages that shouldn't get compiled in normally.
+ *)
+(** to be translated if needed
+#ifdef DEBUG
+#    define av_dlog(pctx, ...) av_log(pctx, AV_LOG_DEBUG, __VA_ARGS__)
+#else
+#    define av_dlog(pctx, ...) do { if (0) av_log(pctx, AV_LOG_DEBUG, __VA_ARGS__); } while (0)
+#endif
+**)
+
+(**
+ * Skip repeated messages, this requires the user app to use av_log() instead of
+ * (f)printf as the 2 would otherwise interfere and lead to
+ * "Last message repeated x times" messages below (f)printf messages with some
+ * bad luck.
+ * Also to receive the last, "last repeated" line if any, the user app must
+ * call av_log(NULL, AV_LOG_QUIET, "%s", ""); at the end
+ *)
+const
+  AV_LOG_SKIP_REPEATED = 1;
+
+procedure av_log_set_flags(arg: cint);
+  cdecl; external av__util;
