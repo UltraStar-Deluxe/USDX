38ffc9db419c2d351e0de14581e322c79faee00d
brianch <brianch@users.noreply.github.com>
2014-12-26 22:17:56 +0000

    Fix FF_API defines for ffmpeg 2.4, it should work now.
    git-svn-id: https://svn.code.sf.net/p/ultrastardx/svn/trunk@3108 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- src/lib/ffmpeg-2.4/avformat.pas
+++ src/lib/ffmpeg-2.4/avformat.pas
@@ -94,40 +94,6 @@ const
 }
 
 (**
- * FF_API_* defines may be placed below to indicate public API that will be
- * dropped at a future version bump. The defines themselves are not part of
- * the public API and may change, break or disappear at any time.
- *)
-
-{$ifndef FF_API_REFERENCE_DTS}
-{$define FF_API_REFERENCE_DTS         := (LIBAVFORMAT_VERSION_MAJOR < 56)}
-{$endif}  
-{$ifndef FF_API_ALLOC_OUTPUT_CONTEXT}
-{$define FF_API_ALLOC_OUTPUT_CONTEXT  := (LIBAVFORMAT_VERSION_MAJOR < 56)}
-{$endif}
-{$ifndef FF_API_FORMAT_PARAMETERS}
-{$define FF_API_FORMAT_PARAMETERS     := (LIBAVFORMAT_VERSION_MAJOR < 56)}
-{$endif}
-{$ifndef FF_API_NEW_STREAM}
-{$define FF_API_NEW_STREAM            := (LIBAVFORMAT_VERSION_MAJOR < 56)}
-{$endif}
-{$ifndef FF_API_SET_PTS_INFO}
-{$define FF_API_SET_PTS_INFO          := (LIBAVFORMAT_VERSION_MAJOR < 56)}
-{$endif}
-{$ifndef FF_API_CLOSE_INPUT_FILE}
-{$define FF_API_CLOSE_INPUT_FILE      := (LIBAVFORMAT_VERSION_MAJOR < 56)}
-{$endif}
-{$ifndef FF_API_READ_PACKET}
-{$define FF_API_READ_PACKET           := (LIBAVFORMAT_VERSION_MAJOR < 56)}
-{$endif}
-{$ifndef FF_API_ASS_SSA}
-{$define FF_API_ASS_SSA               := (LIBAVFORMAT_VERSION_MAJOR < 56)}
-{$endif}
-{$ifndef FF_API_R_FRAME_RATE}
-{$define FF_API_R_FRAME_RATE          := 1}
-{$endif}
-
-(**
  * @defgroup libavf I/O and Muxing/Demuxing Library
  * @
  *
