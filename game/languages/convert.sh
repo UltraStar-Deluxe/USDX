23ae943068292510cff4c890903d913759ac95eb
RattleSN4K3 <RattleSN4K3@gmail.com>
2016-07-27 01:50:21 +0200

    Convert ini files, auto correct to UTF8
    It should now also support files with spaces. If a file cannot be converted it is kept (and not overwritten), the log file named by the error file can be checked.

--- game/languages/convert.sh
+++ game/languages/convert.sh
@@ -1,43 +1,71 @@
 #!/bin/bash
 # See: http://www.microsoft.com/resources/msdn/goglobal/default.mspx?OS=Windows%20Vista
 
+declare -a forceLang=()
+declare -a forceCP=()
+
 function convertUTF8 {
-  if [ ! -f $2.ini ]; then
-    echo "skip $2.ini: does not exist"
+  local fromcp;
+  local fromcptext;
+  if [ ! -f "$1" ]; then
+    echo "skip $1: does not exist"
     return
+  fi
+  
+  fromcp=`getCP "$1"`
+  if [ -n "$fromcp" ]; then
+    fromcptext=" from $fromcp";
+	fromcp=" -f $fromcp"
   fi
 
   # UTF-8 BOM (0xEF 0xBB 0xBF)
   BOM=`echo -n -e "\0357\0273\0277"`
-  HEADER=`head -c3 $2.ini`
+  HEADER=`head -c3 "$1"`
   if [ $HEADER != $BOM ]; then
-    echo "Convert $2.ini from $1 to UTF8"
-    echo -n $BOM >$2.tmp
-    iconv -f $1 -t UTF-8 $2.ini >>$2.tmp
-    mv $2.tmp $2.ini
+    echo -n "Convert $1$fromcptext to UTF8"
+    echo -n $BOM >"$1.tmp"
+    ERROR=$(iconv $fromcp -t UTF-8 "$1" 2>&1 >> "$1.tmp" )
+    
+    if [ -z "$ERROR" ]; then
+      mv "$1.tmp" "$1"
+      echo "... done."
+    else
+      # file wasn't converted propertly
+      rm -f "$1.tmp"
+      echo ". Error, unable to convert. Check $1.log"
+      echo $ERROR>"$1.log"
+    fi
   else
-    echo "skip $2.ini: already UTF8"    
+    echo "skip $1: already UTF8"    
   fi
 }
 
-#convertUTF8 UTF8 Catalan
-convertUTF8 CP1252 French 
-convertUTF8 CP1252 Italian 
-convertUTF8 CP1252 Spanish 
-convertUTF8 CP1250 Croatian
-convertUTF8 CP1252 Euskara 
-convertUTF8 CP1252 German 
-#convertUTF8 UCS-2LE Japanese 
-convertUTF8 CP1252 Swedish 
-convertUTF8 CP1252 Dutch 
-convertUTF8 CP1252 Finnish 
-convertUTF8 CP1253 Greek 
-convertUTF8 CP1252 Portuguese 
+function Force() {
+  forceLang+=($1.ini)
+  forceCP+=($2)
+}
 
-convertUTF8 CP1252 Danish 
-convertUTF8 CP1252 Norwegian 
-#convertUTF8 CP1251 Serbian 
-convertUTF8 CP1250 Serbian 
-convertUTF8 CP1250 Slovenian 
-convertUTF8 CP1250 Polish 
-convertUTF8 CP1250 Slovak 
+function getCP () {
+  local e
+  local found
+  local value=$1
+  
+  i=0
+  for e in "${forceLang[@]}"
+  do
+    if [ "$e" == "$value" ] ; then
+      echo ${forceCP[$i]}
+      return 1
+    fi
+    let i=i+1
+  done
+  
+  return 0
+}
+
+# Enable forcing a specific codepage to convert from
+# add likes like the following to do so
+#Force "Spanish" "CP1252" 
+
+for f in *.ini; do convertUTF8 "$f"; done
+convertUTF8 Language.new
