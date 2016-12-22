f6751393f19d53d18d20429de76a01011fb813a5
basisbit <hp.stefan@gmail.com>
2016-12-22 15:41:41 +0100

    added song x of y again for tabbed view. fixes #172
    If you get a crash in tabbed song selection because of this change,
    please send us a screenshot of the error message

--- src/screens/UScreenSong.pas
+++ src/screens/UScreenSong.pas
@@ -2283,7 +2283,7 @@ begin
       Text[TextNumber].Text := IntToStr(CatSongs.VisibleIndex(Interaction)+1) + '/' + IntToStr(VS)
     else if (Ini.TabsAtStartup = 1) then
     begin
-      Text[TextNumber].Text := IntToStr(CatSongs.Song[Interaction].CatNumber);
+      Text[TextNumber].Text := IntToStr(CatSongs.Song[Interaction].CatNumber)+ '/' + IntToStr(VS);
       if not Interaction = 0 then Text[TextNumber].Text := Text[TextNumber].Text + '/' + IntToStr(CatSongs.Song[Interaction - CatSongs.Song[Interaction].CatNumber].CatNumber);
     end
     else
