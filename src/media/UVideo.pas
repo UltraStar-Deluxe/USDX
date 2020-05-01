UltraStar-Deluxe/USDX

commit f186fc5bffabce4406d1897c8ef667117ecd11d4

UVideo: Don't delay frames by one frame time
When we add the FrameDelay, we get the time of the next frame and
not the time of the current frame. Only noticeable on very low
frame rate videos

src/media/UVideo.pas
@@ -155,6 +155,7 @@ type
 
     fFrameDuration: extended; //**< duration of a video frame in seconds (= 1/fps)
     fFrameTime: extended; //**< video time position (absolute)
+    fNextFrameTime: extended; //**< video time position (absolute)
     fLoopTime: extended;  //**< start time of the current loop
     fPreferDTS: boolean;
 
@@ -571,6 +572,7 @@ begin
   fPaused := False;
   fFrameDuration := 0;
   fFrameTime := 0;
+  fNextFrameTime := 0;
   fStream := nil;
   fStreamIndex := -1;
   fFrameTexValid := false;
@@ -647,6 +649,7 @@ procedure TVideo_FFmpeg.SynchronizeTime(Frame: PAVFrame; pts: double);
 var
   FrameDelay: double;
 begin
+  fFrameTime := fNextFrameTime;
   if (pts <> 0) then
   begin
     // if we have pts, set video clock to it
@@ -659,7 +662,7 @@ begin
   {$ENDIF}
   // if we are repeating a frame, adjust clock accordingly
   FrameDelay := FrameDelay + Frame^.repeat_pict * (FrameDelay * 0.5);
-  fFrameTime := fFrameTime + FrameDelay;
+  fNextFrameTime := fFrameTime + FrameDelay;
 end;
 
 {**

commit 9e72d373170a832e3603871a5f666b4747994011

UVideo: Fix looping of videos starting at time 0
Looping videos starting at 0s were broken since
f186fc5bffabce4406d1897c8ef667117ecd11d4

When the first frame of the loop has no PTS or a PTS of zero, the value
of fNetFrameTime will be used instead. But that value still was beyond
the end of the loop.

Fixes #520

src/media/UVideo.pas
@@ -1427,6 +1427,7 @@ begin
   SeekFlags := AVSEEK_FLAG_BACKWARD;
 
   fFrameTime := Time;
+  fNextFrameTime := Time;
   fEOF := false;
   fFrameTexValid := false;
 