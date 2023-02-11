c6a76db38b2b8c09ada233a4625e259f5b346bd5
Daniel Glöckner <daniel-gl@gmx.net>
2023-02-11 21:54:18 +0100

    Install aux files when using old autoconf

--- autogen.sh
+++ autogen.sh
@@ -7,4 +7,9 @@ test -n "$srcdir" && cd "$srcdir"
 
 echo "Updating build configuration files for USDX, please wait..."
 
-autoreconf -isf
+autoreconf -isfv
+if ! [ -e dists/autogen/config.sub ] ; then
+	# autoreconf < 2.70 relied on automake to install aux files
+	# but didn't run it in projects that don't use automake macros
+	automake -a 2>&1 | grep installing || true
+fi
