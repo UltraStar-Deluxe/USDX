UltraStar-Deluxe/USDX

commit 983dc2f003466269eb84475defae3815405b1099

control aspect ratio of video and background image simultaneously, and it carries over between songs

src/screens/controllers/UScreenSingController.pas
@@ -155,6 +155,7 @@ type
     PlayerDuetNames:array [1..IMaxPlayerCount] of UTF8String;
 
     Tex_Background: TTexture;
+    // controls both background image and video
     BackgroundAspectCorrection: TAspectCorrection;
     FadeOut: boolean;
 
@@ -303,7 +304,10 @@ begin
           fShowVisualization := false;
           fCurrentVideo := fVideoClip;
           if (Assigned(fCurrentVideo)) then
-               fCurrentVideo.Position := CurrentSong.VideoGAP + CurrentSong.Start + AudioPlayback.Position;
+          begin
+            fCurrentVideo.SetAspectCorrection(BackgroundAspectCorrection);
+            fCurrentVideo.Position := CurrentSong.VideoGAP + CurrentSong.Start + AudioPlayback.Position;
+          end;
           Log.LogStatus('finished switching to video', 'UScreenSing.ParseInput');
         end
         else
@@ -464,16 +468,10 @@ begin
         end
         else
         begin
+          BackgroundAspectCorrection := TAspectCorrection((Ord(BackgroundAspectCorrection)+1) mod (Ord(High(TAspectCorrection))+1));
           if (Assigned(fCurrentVideo)) then
           begin
-             fCurrentVideo.SetAspectCorrection(TAspectCorrection((Ord(fCurrentVideo.GetAspectCorrection())+1) mod (Ord(High(TAspectCorrection))+1)));
-          end
-          else
-          begin
-            if (fShowBackground and CurrentSong.Background.IsSet()) then
-            begin
-              BackgroundAspectCorrection := TAspectCorrection((Ord(BackgroundAspectCorrection)+1) mod (Ord(High(TAspectCorrection))+1));
-            end;
+            fCurrentVideo.SetAspectCorrection(BackgroundAspectCorrection);
           end;
         end;
       end;
@@ -648,6 +646,8 @@ begin
   inherited Create;
   ScreenSing := self;
   screenSingViewRef := TScreenSingView.Create();
+  // for now: default to letterbox but preserve aspect between songs
+  BackgroundAspectCorrection := acoLetterBox;
 
   ClearSettings;
 end;
@@ -1082,6 +1082,7 @@ begin
   begin
     fVideoClip := VideoPlayback.Open(VideoFile);
     fCurrentVideo := fVideoClip;
+    fCurrentVideo.SetAspectCorrection(BackgroundAspectCorrection);
     if (fVideoClip <> nil) then
     begin
       fShowVisualization := false;
@@ -1102,7 +1103,6 @@ begin
     try
       Tex_Background := Texture.LoadTexture(BgFile);
       fShowBackground := fCurrentVideo = nil;
-      BackgroundAspectCorrection := acoLetterBox;
     except
       Log.LogError('Background could not be loaded: ' + BgFile.ToNative);
       Tex_Background.TexNum := 0;

src/media/UVideo.pas
@@ -925,8 +925,6 @@ begin
 
   fPboId := 0;
 
-  fAspectCorrection := acoLetterBox;
-
   fScreen := 1;
 
   fPosX := 0;

commit 55d39135ec65ad31b21c4b5fa471330db43fd52b

Fix crash when video fails to load
Fixes #764.
Broken since 983dc2f003466269eb84475defae3815405b1099.

src/screens/controllers/UScreenSingController.pas
@@ -1087,10 +1087,10 @@ begin
   if (Ini.VideoEnabled = 1) and CurrentSong.Video.IsSet() and VideoFile.IsFile then
   begin
     fVideoClip := VideoPlayback.Open(VideoFile);
-    fCurrentVideo := fVideoClip;
-    fCurrentVideo.SetAspectCorrection(BackgroundAspectCorrection);
     if (fVideoClip <> nil) then
     begin
+      fCurrentVideo := fVideoClip;
+      fCurrentVideo.SetAspectCorrection(BackgroundAspectCorrection);
       fShowVisualization := false;
       if ScreenSong.Mode = smMedley then
         fCurrentVideo.Position := CurrentSong.VideoGAP + MedleyStart