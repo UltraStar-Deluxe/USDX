a0a1de01c79409b19f0cc5adff31860fc7db3669
barbeque-squared <barbeque.squared@gmail.com>
2025-02-01 09:38:40 +0100

    release 2025.2.0

--- src/base/UConfig.pas
+++ src/base/UConfig.pas
@@ -128,7 +128,7 @@ const
    * Current version of UltraStar Deluxe
    *)
    // TODO: inline the contents of ../../VERSION instead
-   USDX_VERSION = '2025.1.0+dev';
+   USDX_VERSION = '2025.2.0';
 
   (*
    * FPC version numbers are already defined as built-in macros:
