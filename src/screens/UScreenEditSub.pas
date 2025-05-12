WLHeyneke/USDX2024

add karaoke mode via #INSTRUMENTALS tag, key to toggle karaoke mode is K while playing.

src/screens/UScreenEditSub.pas
@@ -2792,7 +2792,7 @@ begin
     SelectsS[Interactions[nBut].Num].SelectedOption := SelectsS[Interactions[nBut].Num].SelectedOption -1;
     CurrentSong.Mp3 := Path(SelectsS[Interactions[nBut].Num].TextOptT[SelectsS[Interactions[nBut].Num].SelectedOption]);
     AudioPlayback.Close;
-    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Mp3));
+    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Mp3),nil);
   end;
 
   if ((Mp3SlideId = Interactions[nBut].Num) and (Action = maRight) and (SelectsS[Interactions[nBut].Num].SelectedOption < Length(SelectsS[Interactions[nBut].Num].TextOptT)-1)) then
@@ -2801,7 +2801,7 @@ begin
     SelectsS[Interactions[nBut].Num].SelectedOption := SelectsS[Interactions[nBut].Num].SelectedOption +1;
     CurrentSong.Mp3 := Path(SelectsS[Interactions[nBut].Num].TextOptT[SelectsS[Interactions[nBut].Num].SelectedOption]);
     AudioPlayback.Close();
-    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Mp3));
+    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Mp3),nil);
   end;
 
   if (((VolumeAudioSlideId = Interactions[nBut].Num) or (VolumeMidiSlideId = Interactions[nBut].Num) or (VolumeClickSlideId = Interactions[nBut].Num))
@@ -4933,7 +4933,7 @@ begin
       Tracks[TrackIndex].Lines[0].Notes[0].Color := P1_INVERTED;
     end;
 
-    AudioPlayBack.Open(CurrentSong.Path.Append(CurrentSong.Mp3));
+    AudioPlayBack.Open(CurrentSong.Path.Append(CurrentSong.Mp3),nil);
     //Set Down Music Volume for Better hearability of Midi Sounds
     //Music.SetVolume(0.4);
 

src/screens/UScreenJukebox.pas
@@ -2174,7 +2174,7 @@ procedure TScreenJukebox.Play();
 var
   I: integer;
 begin
-    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Mp3));
+    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Mp3),nil);
     AudioPlayback.SetVolume(1.0);
 
     //AudioPlayback.Position := CurrentSong.Start;

src/screens/UScreenScore.pas
@@ -1827,7 +1827,7 @@ begin
     begin
       AudioPlayback.Close;
 
-      if AudioPlayback.Open(CatSongs.Song[select].Path.Append(CatSongs.Song[select].Mp3)) then
+      if AudioPlayback.Open(CatSongs.Song[select].Path.Append(CatSongs.Song[select].Mp3),nil) then
       begin
         if (CatSongs.Song[select].PreviewStart > 0) then
           AudioPlayback.Position := CatSongs.Song[select].PreviewStart

src/screens/UScreenSong.pas
@@ -3776,7 +3776,7 @@ begin
     Exit;
 
   PlayMidi := false;
-  if AudioPlayback.Open(Song.Path.Append(Song.Mp3)) then
+  if AudioPlayback.Open(Song.Path.Append(Song.Mp3),nil) then
   begin
     PreviewOpened := Interaction;
 

src/screens/controllers/UScreenSingController.pas
@@ -538,6 +538,11 @@ begin
         Pause;
       end;
 
+      SDLK_K:
+      begin
+        AudioPlayback.ToggleKaraoke;
+      end;
+
       SDLK_RIGHT:
       begin
         if (SDL_ModState = KMOD_LCTRL) then // seek 5 seconds forward
@@ -857,7 +862,7 @@ begin
   PlayMidi := false;
   MidiFadeIn := false;
 
-  AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Mp3));
+  AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Mp3),CurrentSong.Path.Append(CurrentSong.Karaoke));
   if ScreenSong.Mode = smMedley then
     AudioPlayback.SetVolume(0.1)
   else

UltraStar-Deluxe/USDX

src/screens/UScreenEditSub.pas
@@ -2798,7 +2798,7 @@ begin
     SelectsS[Interactions[nBut].Num].SelectedOption := SelectsS[Interactions[nBut].Num].SelectedOption -1;
     CurrentSong.Audio := Path(SelectsS[Interactions[nBut].Num].TextOptT[SelectsS[Interactions[nBut].Num].SelectedOption]);
     AudioPlayback.Close;
-    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Audio));
+    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Audio),nil);
   end;
 
   if ((Mp3SlideId = Interactions[nBut].Num) and (Action = maRight) and (SelectsS[Interactions[nBut].Num].SelectedOption < Length(SelectsS[Interactions[nBut].Num].TextOptT)-1)) then
@@ -2807,7 +2807,7 @@ begin
     SelectsS[Interactions[nBut].Num].SelectedOption := SelectsS[Interactions[nBut].Num].SelectedOption +1;
     CurrentSong.Audio := Path(SelectsS[Interactions[nBut].Num].TextOptT[SelectsS[Interactions[nBut].Num].SelectedOption]);
     AudioPlayback.Close();
-    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Audio));
+    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Audio),nil);
   end;
 
   if (((VolumeAudioSlideId = Interactions[nBut].Num) or (VolumeMidiSlideId = Interactions[nBut].Num) or (VolumeClickSlideId = Interactions[nBut].Num))
@@ -4921,7 +4921,7 @@ begin
       Tracks[TrackIndex].Lines[0].Notes[0].Color := P1_INVERTED;
     end;
 
-    AudioPlayBack.Open(CurrentSong.Path.Append(CurrentSong.Audio));
+    AudioPlayBack.Open(CurrentSong.Path.Append(CurrentSong.Audio),nil);
     //Set Down Music Volume for Better hearability of Midi Sounds
     //Music.SetVolume(0.4);
 

src/screens/UScreenJukebox.pas
@@ -2174,7 +2174,7 @@ procedure TScreenJukebox.Play();
 var
   I: integer;
 begin
-    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Audio));
+    AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Audio),nil);
     AudioPlayback.SetVolume(1.0);
 
     //AudioPlayback.Position := CurrentSong.Start;

src/screens/UScreenScore.pas
@@ -1792,7 +1792,7 @@ begin
     begin
       AudioPlayback.Close;
 
-      if AudioPlayback.Open(CatSongs.Song[select].Path.Append(CatSongs.Song[select].Audio)) then
+      if AudioPlayback.Open(CatSongs.Song[select].Path.Append(CatSongs.Song[select].Audio),nil) then
       begin
         if (CatSongs.Song[select].PreviewStart > 0) then
           AudioPlayback.Position := CatSongs.Song[select].PreviewStart

src/screens/UScreenSong.pas
@@ -3710,7 +3710,7 @@ begin
     Exit;
 
   PlayMidi := false;
-  if AudioPlayback.Open(Song.Path.Append(Song.Audio)) then
+  if AudioPlayback.Open(Song.Path.Append(Song.Audio),nil) then
   begin
     PreviewOpened := Interaction;
 

src/screens/controllers/UScreenSingController.pas
@@ -551,6 +551,11 @@ begin
         Pause;
       end;
 
+      SDLK_K:
+      begin
+        AudioPlayback.ToggleKaraoke;
+      end;
+
       SDLK_RIGHT:
       begin
         if (SDL_ModState = KMOD_LCTRL) then // seek 5 seconds forward
@@ -870,7 +875,7 @@ begin
   PlayMidi := false;
   MidiFadeIn := false;
 
-  AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Audio));
+  AudioPlayback.Open(CurrentSong.Path.Append(CurrentSong.Audio),CurrentSong.Path.Append(CurrentSong.Karaoke));
   if ScreenSong.Mode = smMedley then
     AudioPlayback.SetVolume(0.1)
   else