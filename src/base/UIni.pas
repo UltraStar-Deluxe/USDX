83d1cd180841f41b972c7fc481742baa0b0e65db
Daniel Gruss <daniel.gruss@tugraz.at>
2025-12-13 15:08:18 +0100

    lazy load song notes

--- src/base/UIni.pas
+++ src/base/UIni.pas
@@ -219,6 +219,7 @@ type
       SingTimebarMode:       integer;
       JukeboxTimebarMode:    integer;
       DefaultSingMode:       integer;
+      PreloadSongNotes: integer;
 
       // Controller
       Joypad:         integer;
@@ -451,6 +452,7 @@ const
   sSelectPlayer = 1;
   sOpenMenu = 2;
   IDefaultSingMode: array[0..1] of UTF8String = ('Regular', 'Instrumental');
+  IPreloadSongNotes: array[0..1] of UTF8String = ('Off', 'On');
 
   IPartyPopup:    array[0..1] of UTF8String = ('Off', 'On');
   IAutoSongFileSearch: array[0..1] of UTF8String = ('Off', 'On');
@@ -553,6 +555,7 @@ var
   ISingScoresTranslated:       array[0..1] of UTF8String = ('Off', 'On');
   ITopScoresTranslated:        array[0..1] of UTF8String = ('All', 'Player');
   IDefaultSingModeTranslated:  array[0..1] of UTF8String = ('Regular', 'Instrumental');
+  IPreloadSongNotesTranslated: array[0..1] of UTF8String = ('Off', 'On');
 
   IJoypadTranslated:           array[0..1] of UTF8String = ('Off', 'On');
   IMouseTranslated:            array[0..2] of UTF8String = ('Off', 'On [System Cursor]', 'On [Game Cursor]');
@@ -831,6 +834,9 @@ begin
 
   IDefaultSingModeTranslated[0]       := ULanguage.Language.Translate('OPTION_VALUE_REGULAR');
   IDefaultSingModeTranslated[1]       := ULanguage.Language.Translate('OPTION_VALUE_INSTRUMENTAL');
+
+  IPreloadSongNotesTranslated[0]      := ULanguage.Language.Translate('OPTION_VALUE_OFF');
+  IPreloadSongNotesTranslated[1]      := ULanguage.Language.Translate('OPTION_VALUE_ON');
 
   IPartyPopupTranslated[0]            := ULanguage.Language.Translate('OPTION_VALUE_OFF');
   IPartyPopupTranslated[1]            := ULanguage.Language.Translate('OPTION_VALUE_ON');
@@ -1606,6 +1612,9 @@ begin
   // DefaultSingMode
   DefaultSingMode := ReadArrayIndex(IDefaultSingMode, IniFile, 'Advanced', 'DefaultSingMode', IGNORE_INDEX, 'Regular');
 
+  // PreloadSongNotes
+  PreloadSongNotes := ReadArrayIndex(IPreloadSongNotes, IniFile, 'Advanced', 'PreloadSongNotes', IGNORE_INDEX, 'On');
+
   // PartyPopup
   PartyPopup := ReadArrayIndex(IPartyPopup, IniFile, 'Advanced', 'PartyPopup', IGNORE_INDEX, 'On');
 
@@ -1927,6 +1936,9 @@ begin
 
     //DefaultSingMode
     IniFile.WriteString('Advanced', 'DefaultSingMode', IDefaultSingMode[DefaultSingMode]);
+
+    // PreloadSongNotes
+    IniFile.WriteString('Advanced', 'PreloadSongNotes', IPreloadSongNotes[PreloadSongNotes]);
 
     //Party Popup
     IniFile.WriteString('Advanced', 'PartyPopup', IPartyPopup[PartyPopup]);
