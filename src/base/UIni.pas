aebc949a3ab66fdae4a9a433e55223b64397236b
Daniel Gruss <daniel.gruss@tugraz.at>
2025-12-15 19:26:34 +0100

    add automatic search for background and cover

--- src/base/UIni.pas
+++ src/base/UIni.pas
@@ -216,6 +216,7 @@ type
       PartyPopup:     integer;
       SingScores:     integer;
       TopScores:      integer;
+      AutoSongFileSearch: integer;
       SingTimebarMode:       integer;
       JukeboxTimebarMode:    integer;
       DefaultSingMode:       integer;
@@ -456,6 +457,7 @@ const
 
   ILineBonus:     array[0..1] of UTF8String = ('Off', 'On');
   IPartyPopup:    array[0..1] of UTF8String = ('Off', 'On');
+  IAutoSongFileSearch: array[0..1] of UTF8String = ('Off', 'On');
 
   IJoypad:        array[0..1] of UTF8String = ('Off', 'On');
   IMouse:         array[0..2] of UTF8String = ('Off', 'System', 'Game');
@@ -557,6 +559,7 @@ var
   ITopScoresTranslated:        array[0..1] of UTF8String = ('All', 'Player');
   IDefaultSingModeTranslated:  array[0..1] of UTF8String = ('Regular', 'Instrumental');
   IPreloadSongNotesTranslated: array[0..1] of UTF8String = ('Off', 'On');
+  IAutoSongFileSearchTranslated: array[0..1] of UTF8String = ('Off', 'On');
 
   IJoypadTranslated:           array[0..1] of UTF8String = ('Off', 'On');
   IMouseTranslated:            array[0..2] of UTF8String = ('Off', 'On [System Cursor]', 'On [Game Cursor]');
@@ -843,6 +846,8 @@ begin
   IPreloadSongNotesTranslated[0]      := ULanguage.Language.Translate('OPTION_VALUE_OFF');
   IPreloadSongNotesTranslated[1]      := ULanguage.Language.Translate('OPTION_VALUE_ON');
 
+  IAutoSongFileSearchTranslated[0]    := ULanguage.Language.Translate('OPTION_VALUE_OFF');
+  IAutoSongFileSearchTranslated[1]    := ULanguage.Language.Translate('OPTION_VALUE_ON');
 
   ILineBonusTranslated[0]             := ULanguage.Language.Translate('OPTION_VALUE_OFF');
   ILineBonusTranslated[1]             := ULanguage.Language.Translate('OPTION_VALUE_ON');
@@ -1636,6 +1641,10 @@ begin
   // TopScores
   TopScores := ReadArrayIndex(ITopScores, IniFile, 'Advanced', 'TopScores', IGNORE_INDEX, 'Player');
 
+  // AutoSongFileSearch
+  AutoSongFileSearch := ReadArrayIndex(IAutoSongFileSearch, IniFile, 'Advanced', 'AutoSongFileSearch', IGNORE_INDEX, 'Off');
+  if (AutoSongFileSearch < 0) or (AutoSongFileSearch > High(IAutoSongFileSearch)) then
+    AutoSongFileSearch := 1;
 
   // SyncTo
   SyncTo := ReadArrayIndex(ISyncTo, IniFile, 'Advanced', 'SyncTo', Ord(stMusic));
@@ -1960,6 +1969,8 @@ begin
     //TopScores
     IniFile.WriteString('Advanced', 'TopScores', ITopScores[TopScores]);
 
+    // AutoSongFileSearch
+    IniFile.WriteString('Advanced', 'AutoSongFileSearch', IAutoSongFileSearch[AutoSongFileSearch]);
 
     //SyncTo
     IniFile.WriteString('Advanced', 'SyncTo', ISyncTo[SyncTo]);
