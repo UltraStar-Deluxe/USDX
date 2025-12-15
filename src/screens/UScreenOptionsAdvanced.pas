13d430f7d4dd5617836fa4f11deb3bb039ae8a00
Daniel Gruss <daniel.gruss@tugraz.at>
2025-12-15 19:12:46 +0100

    remove automatic search for background and cover

--- src/screens/UScreenOptionsAdvanced.pas
+++ src/screens/UScreenOptionsAdvanced.pas
@@ -150,7 +150,6 @@ begin
   AddSelectSlide('SING_OPTIONS_ADVANCED_EFFECTSING', Ini.EffectSing, IEffectSingTranslated);
   AddSelectSlide('SING_OPTIONS_GRAPHICS_LINEBONUS', Ini.LineBonus, ILineBonusTranslated);
   AddSelectSlide('SING_OPTIONS_ADVANCED_ONSONGCLICK', Ini.OnSongClick, IOnSongClickTranslated);
-  AddSelectSlide('SING_OPTIONS_ADVANCED_AUTOSONGFILESEARCH', Ini.AutoSongFileSearch, IAutoSongFileSearchTranslated);
   AddSelectSlide('SING_OPTIONS_ADVANCED_PRELOAD_SONG_NOTES', Ini.PreloadSongNotes, IPreloadSongNotesTranslated);
   AddSelectSlide('SING_OPTIONS_ADVANCED_ASKBEFOREDEL', Ini.AskBeforeDel, IAskbeforeDelTranslated);
   AddSelectSlide('SING_OPTIONS_ADVANCED_PARTYPOPUP', Ini.PartyPopup, IPartyPopupTranslated);
