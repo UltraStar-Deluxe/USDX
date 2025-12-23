d4d18a45ad69d0d9f7118765a2b34300935a0bdd
Daniel Gruss <daniel.gruss@tugraz.at>
2025-12-23 23:24:32 +0100

    make duets go into highscore - add config screen and config support for duet scores - do not add duet scores for non-duet songs - fix highscore P1/P2 display

--- src/screens/UScreenScore.pas
+++ src/screens/UScreenScore.pas
@@ -378,7 +378,7 @@ begin
         begin
           if (FinishScreenDraw = true) then
           begin
-            if (CurrentSong.isDuet) or (ScreenSong.RapToFreestyle) or (ScreenSong.Mode = smMedley) then
+            if (CurrentSong.isDuet and (Ini.DuetScores = 0)) or (ScreenSong.RapToFreestyle) or (ScreenSong.Mode = smMedley) then
               FadeTo(@ScreenSong)
             else
               FadeTo(@ScreenTop5);
@@ -402,7 +402,7 @@ begin
            if (FinishScreenDraw = true) then
            begin
 
-             if (CurrentSong.isDuet) or (ScreenSong.RapToFreestyle) or (ScreenSong.Mode = smMedley) then
+             if (CurrentSong.isDuet and (Ini.DuetScores = 0)) or (ScreenSong.RapToFreestyle) or (ScreenSong.Mode = smMedley) then
                FadeTo(@ScreenSong)
              else
                FadeTo(@ScreenTop5);
