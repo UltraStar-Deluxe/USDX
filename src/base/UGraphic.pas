82a6c3bd10c11f169debabeec82cf43daf1db41e
Daniel Gruss <daniel.gruss@tugraz.at>
2026-01-17 12:48:46 +0100

    implement a scaling mechanism that allows themes to scale from 4:3 to 16:9 without distortion of fonts and images

--- src/base/UGraphic.pas
+++ src/base/UGraphic.pas
@@ -93,7 +93,9 @@ uses
   UScreenStatMain,
   UScreenStatDetail,
   {Popup for errors, etc.}
-  UScreenPopup;
+  UScreenPopup,
+  UThemes,
+  UScale;
 
 type
   TRecR = record
@@ -698,10 +700,19 @@ NoDoubledResolution:
     SoftwareRendering := false;
 
   // define virtual (Render) and real (Screen) screen size
-  RenderW := 800;
-  RenderH := 600;
+  if Theme <> nil then
+  begin
+    RenderW := Theme.LayoutScreenW;
+    RenderH := Theme.LayoutScreenH;
+  end
+  else
+  begin
+    RenderW := 800;
+    RenderH := 600;
+  end;
   ScreenW := ActualW;
   ScreenH := ActualH;
+  UpdateUIScaleState(RenderW, RenderH, ScreenW, ScreenH);
   // Ausganswerte für die State-Machine setzen
   SDL_GL_SetSwapInterval(1); // VSYNC (currently Windows only)
 
@@ -860,6 +871,8 @@ begin
       LastH := h;
     end;
   end;
+
+  UpdateUIScaleState(RenderW, RenderH, ScreenW, ScreenH);
 
   if CurrentWindowMode = Mode_Fullscreen then
   begin
