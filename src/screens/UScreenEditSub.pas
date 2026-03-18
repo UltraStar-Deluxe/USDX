344c2b60e17cd30268f590b7129ba672d5780433
Daniel Gruss <daniel.gruss@tugraz.at>
2026-03-18 08:59:15 +0100

    add missing wiring back to Ini for persistency

--- src/screens/UScreenEditSub.pas
+++ src/screens/UScreenEditSub.pas
@@ -6320,6 +6320,10 @@ end;
 
 procedure TScreenEditSub.OnHide;
 begin
+  Ini.EditorClickLeadMs := PrerollAudioIndex - 500;
+  Ini.EditorMidiLeadMs := PrerollMidiIndex - 500;
+  Ini.Save;
+
   {$IFDEF UseMIDIPort}
   StopMidi;
   MidiOut.Close;
