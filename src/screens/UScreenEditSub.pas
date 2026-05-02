da5a7d817007111978033970ceeb4fb8a70e5394
Daniel Gruss <daniel.gruss@tugraz.at>
2026-05-02 18:27:01 +0200

    midi volume control should be immediate and include midi emulation

--- src/screens/UScreenEditSub.pas
+++ src/screens/UScreenEditSub.pas
@@ -435,6 +435,7 @@ type
       procedure ResetMidiLastNote;
       procedure ReleaseMidiLastNote;
       procedure StopMidiPlayback;
+      procedure ApplyMidiVolume;
       {$ENDIF}
 
     public
@@ -1037,6 +1038,13 @@ begin
   end;
 
   ResetMidiLastNote;
+end;
+
+procedure TScreenEditSub.ApplyMidiVolume;
+begin
+  if Assigned(MidiOut) and
+     (VolumeMidiSlideId >= 0) and (VolumeMidiSlideId <= High(SelectsS)) then
+    MidiOut.PutShort($B1, $7, Floor(1.27 * EnsureRange(SelectsS[VolumeMidiSlideId].SelectedOption, 0, 100)));
 end;
 {$ENDIF}
 
@@ -2875,6 +2883,7 @@ var
   TempR:  real;
   i:      Integer;
   PrevAudioIndex: Integer;
+  PrevMidiIndex: Integer;
   PrevClickIndex: Integer;
   CursorWordIndex: Integer;
   CursorCharIndex: Integer;
@@ -2889,6 +2898,7 @@ begin
   CurrentY := Y;
 
   PrevAudioIndex := VolumeAudioIndex;
+  PrevMidiIndex := VolumeMidiIndex;
   PrevClickIndex := VolumeClickIndex;
 
   Result := true;
@@ -3187,6 +3197,11 @@ begin
 
   if (VolumeAudioSlideId = Interactions[nBut].Num) and (VolumeAudioIndex <> PrevAudioIndex) then
     SetAudioVolumePercent(EnsureRange(VolumeAudioIndex, 0, 100));
+
+  {$IFDEF UseMIDIPort}
+  if (VolumeMidiSlideId = Interactions[nBut].Num) and (VolumeMidiIndex <> PrevMidiIndex) then
+    ApplyMidiVolume;
+  {$ENDIF}
 
   if (VolumeClickSlideId = Interactions[nBut].Num) and (VolumeClickIndex <> PrevClickIndex) then
     SetSfxVolumePercent(EnsureRange(VolumeClickIndex, 0, 100));
