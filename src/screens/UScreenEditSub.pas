0163af1e20db7ab7fa539077158a3206d74f1b70
Daniel Gruss <daniel.gruss@tugraz.at>
2026-05-02 21:14:03 +0200

    save and restore midi volume via ini

--- src/screens/UScreenEditSub.pas
+++ src/screens/UScreenEditSub.pas
@@ -1034,7 +1034,10 @@ procedure TScreenEditSub.ApplyMidiVolume;
 begin
   if Assigned(MidiOut) and
      (VolumeMidiSlideId >= 0) and (VolumeMidiSlideId <= High(SelectsS)) then
-    MidiOut.PutShort($B1, $7, Floor(1.27 * EnsureRange(SelectsS[VolumeMidiSlideId].SelectedOption, 0, 100)));
+  begin
+    Ini.MidiVolume := EnsureRange(SelectsS[VolumeMidiSlideId].SelectedOption, 0, 100);
+    MidiOut.PutShort($B1, $7, Floor(1.27 * Ini.MidiVolume));
+  end;
 end;
 {$ENDIF}
 
@@ -3207,7 +3210,10 @@ begin
 
   {$IFDEF UseMIDIPort}
   if (VolumeMidiSlideId = Interactions[nBut].Num) and (VolumeMidiIndex <> PrevMidiIndex) then
+  begin
     ApplyMidiVolume;
+    Ini.Save;
+  end;
   {$ENDIF}
 
   if (VolumeClickSlideId = Interactions[nBut].Num) and (VolumeClickIndex <> PrevClickIndex) then
@@ -5748,7 +5754,7 @@ begin
       VolumeClick[VolumeIndex] := IntToStr(VolumeIndex);
     end;
     VolumeAudioIndex := EnsureRange(Ini.AudioVolume, 0, 100);
-    VolumeMidiIndex  := 100;
+    VolumeMidiIndex  := EnsureRange(Ini.MidiVolume, 0, 100);
     VolumeClickIndex := EnsureRange(Ini.SfxVolume, 0, 100);
     UpdateSelectSlideOptions(VolumeAudioSlideId, VolumeAudio, VolumeAudioIndex);
     UpdateSelectSlideOptions(VolumeMidiSlideId,  VolumeMidi,  VolumeMidiIndex);
@@ -5850,6 +5856,7 @@ end;
 procedure TScreenEditSub.SyncVolumeSlidersFromIni;
 var
   NewAudio: Integer;
+  NewMidi: Integer;
   NewClick: Integer;
 begin
   NewAudio := EnsureRange(Ini.AudioVolume, 0, 100);
@@ -5858,6 +5865,14 @@ begin
   begin
     VolumeAudioIndex := NewAudio;
     SelectsS[VolumeAudioSlideId].SelectedOption := VolumeAudioIndex;
+  end;
+
+  NewMidi := EnsureRange(Ini.MidiVolume, 0, 100);
+  if (VolumeMidiIndex <> NewMidi) and
+     (VolumeMidiSlideId >= 0) and (VolumeMidiSlideId <= High(SelectsS)) then
+  begin
+    VolumeMidiIndex := NewMidi;
+    SelectsS[VolumeMidiSlideId].SelectedOption := VolumeMidiIndex;
   end;
 
   NewClick := EnsureRange(Ini.SfxVolume, 0, 100);
