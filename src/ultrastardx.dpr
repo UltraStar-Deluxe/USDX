5c60c64e6de6e5af90ea62c27f643e5a512c5789
Daniel Gruss <daniel.gruss@tugraz.at>
2026-05-12 06:21:19 +0200

    Add player layout helper

--- src/ultrastardx.dpr
+++ src/ultrastardx.dpr
@@ -215,6 +215,7 @@ uses
   UPathUtils        in 'base\UPathUtils.pas',
   UNote             in 'base\UNote.pas',
   UBeatTimer        in 'base\UBeatTimer.pas',
+  UPlayerLayout     in 'base\UPlayerLayout.pas',
 
   TextGL            in 'base\TextGL.pas',
   UUnicodeUtils     in 'base\UUnicodeUtils.pas',
